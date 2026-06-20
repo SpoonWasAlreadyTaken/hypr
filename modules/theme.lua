00bcfe7be4f5edc7ebb89b536a35db594b685908
Spoon <121692985+SpoonWasAlreadyTaken@users.noreply.github.com>
2026-06-20 18:30:52 +0300

    Update theme.lua

--- modules/theme.lua
+++ modules/theme.lua
@@ -12,7 +12,7 @@ hl.config({
         border_size = 1,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = { colors = {"rgba(e97dffff)", "rgba(ff005dff)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
